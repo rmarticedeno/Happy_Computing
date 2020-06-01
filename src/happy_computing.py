90747dffe49c9c9ab8c3ae6e064ed219e33b3b6f
rmarticedeno <rmarticedeno@gmail.com>
2020-06-01 17:20:46 -0400

    New approach

--- src/happy_computing.py
+++ src/happy_computing.py
@@ -2,6 +2,8 @@ import random
 import math
 import queue 
 
+MAX = 960
+
 def exponential(lmda):
     val = random.random()
     return -1/lmda * (math.log(val))
@@ -10,7 +12,7 @@ def normal01():
     y = 0
     while True:
         y1 = exponential(1)
-        y2 = exponential(1)
+        y2 = exponential(2)
         y = y2 - (y1 - 1)**2/2
         if y > 0:
             break
@@ -30,148 +32,184 @@ def generate_client_type():
         return 4
     return 3
 
-def generate_client(t, lmda):
-    #return t - 1/lmda*(math.log(random.random()))   
-    return t + exponential(lmda)
+attender_type = {1:'tec', 2:'tec', 3:'stec', 4:'none'}
+
+def generate_client(t, lmda=1/20):
+    return t - 1/lmda*(math.log(random.random()))   
+
+def generate_vendor_time(miu=10, ro=5):
+    return abs(normal(miu,ro))
+
+def generate_tec_time(lmbda=1/20):
+    return exponential(lmbda)
+
+
+class Client:
+    def __init__(self, number = -1, arrival = MAX):
+        self.arrival = arrival
+        self.id = number
+        self.vendor_waiting_time = 0
+        self.stec_waiting_time = 0
+        self.tec_waiting_time = 0
+        self.vendor_end_time = 0
+        self.tec_end_time = 0
+        self.finish_time = 0
+        self.vendor_id = 0
+        self.tec_id = 0
+        self.type = generate_client_type()
+        self.expenses = 0
+
+    def __eq__(self, other):
+        return max(self.arrival,self.vendor_end_time,self.tec_end_time) == max(other.arrival, other.vendor_end_time, other.tec_end_time)
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    def __lt__(self, other):
+        return max(self.arrival,self.vendor_end_time,self.tec_end_time) < max(other.arrival, other.vendor_end_time, other.tec_end_time)
+    def __ge__(self, other):
+        return not self.__lt__(other)
+    def __gt__(self, other):
+        return max(self.arrival,self.vendor_end_time,self.tec_end_time) > max(other.arrival, other.vendor_end_time, other.tec_end_time)
+    def __le__(self, other):
+        return not self.__gt__(other)
+
+
 
 class HappyComputing:
-    def __init__(self, uptime, vendors, tec, stec, arrival ,vendor_lambda, tec_lambda, stec_lambda, infinite=None):
-        self.T = uptime #Shop working time
-        self.n_vendors = vendors #Number of vendors
-        self.n_tec = tec # tecs number
-        self.n_stec = stec # special tecs number
-        self.arrival = arrival # lamda of arrival time
-        self.infinite = 2*self.T if infinite is None else infinite # Considerable large number to avoid errors
-        self.reset() 
+    def __init__(self, uptime=480, vendors=2, tec=3, stec=1, arrival_lambda = 1/20 ,vendor_lambda = (5,2), tec_lambda = 1/20, stec_lambda = 1/15, infinite=None):
+        self.arrival_list = []
+        self.vendor_list = []
+        self.tec_list = []
+        self.stec_list = []
+        self.visitors_list = []
+        self.vendor_number = vendors
+        self.tec_number = tec
+        self.stec_number = stec
+        self.system_time = 0
+
+        self.T = uptime
+        self.MAX = 2*self.T if infinite is None else infinite
+
+        self.SS = []
         
 
-    def reset(self):
-        self.SS = [0] * (self.n_vendors + self.n_tec + self.n_stec + 1) # Sistem state [0] number of costumers in the system, 
-        # [i] in [1, self.n_vendor + 1] (vendors), i in [self.n_vendors + 2, self.n_vendors + self.n_tex + 2] (tecs), rest (spec tecs)
-        self.EL = [ self.infinite ] * (self.n_vendors + self.n_tec + self.n_stec + 1) #Event list [0] next arrival [i] i>0 service completion time of vendor i
-        self.CS = [0] * self.n_vendors #Number of costumers served by each vendor
-        self.A = [] #arrival time of costumers
-        self.D = [] #depature time of costumers 
-        self.Tp = 0 #Time after work sesion limit
-        self.t = 0 #Simulation time
-        self.NA = 0 #Arrival count
-        self.ND = 0 #Depature count
-        self.profit = 0 #earnings of the day
-        self.Stec = queue.Queue() # Special Tec waiting queue
-        self.Tec = queue.Queue() # Regula Tec waiting queue
-
     def simulate(self):
-        self.reset()
+        self.SS = [Client()]*(self.vendor_number + self.tec_number + self.stec_number + 1)
+
+        t = 0
+        i = 1
+        while(t < self.T):
+            t = generate_client(t)
+            self.arrival_list.append(Client(i, t))
+            i+=1
+        
+        self.SS[0].append(self.arrival_list.pop(0))
+
         self._simulate()
 
     def _simulate(self):
-        #Generar el 1mer cliente
-        self.EL[0] = generate_client(self.t, self.arrival)
-        while(True):
-            ta = self.EL[0]
-            tv = min(*self.EL[1:self.n_vendors + 1])
-            ttec = min(*self.EL[self.n_vendors + 1: self.n_vendors + self.n_tec + 1])
-            tstec = min(*self.EL[self.n_vendors + self.n_tec + 1:])
-            ti = min(*self.EL)
-            T = self.T
-            n = self.SS[0]
-            if ta == min(*self.EL) and ta <= T:
-                #a new customer arrives
-                self.t = ta
-                self.NA += 1
-                #print(f"Customer {self.NA} arrives at {self.t}")
-                self.A.append((self.NA, self.t))
-                # generar el próximo cliente
-                self.EL[0] = generate_client(self.t, self.arrival)
-                if self.EL[0] > T:
-                    self.EL[0] = self.infinite
-                #agregarlo a un vededor si es posible
-                free = self._find_free_worker()
-                if free:
-                    choice = random.choice(free)
-                    #print(f"{choice} va a atender al cliente {self.NA}")
-                    self.SS[choice] = self.NA
-                    self.EL[choice] = self.t + self._generate_vendor_time()
-                    #print(f"{self.NA} se retira en el momento {self.EL[choice]}")
-                self.SS[0] += 1
-                continue
-            elif ti < self.infinite and ta != ti:
-                #a customer leaves
-                # _min es el indice del servidor que termina antes
-                _min = [i for i in range(1,len(self.EL)) if self.EL[i] == ti][0]
-                #print(f"Customer {self.SS[_min]} will leave at {ti}")
-                self.D.append((self.SS[_min], ti))
-                self.t = ti
-                self.CS[_min-1] += 1
-                self.ND += 1
-                #self.D[_min-1] = self.t
-                self.EL[_min] = self.infinite
-                self.SS[_min] = 0
-                # there are customers waiting
-                if n > self.n_vendors:
-                    self.SS[_min] = max(*self.SS[1:]) + 1
-                    self.EL[_min] = self.t + self._generate_vendor_time()
-                self.SS[0] -= 1 
-            else:
-                break
-        
+        while(len(self.arrival_list) + len(self.tec_list) + len(self.stec_list)):
+            arriv = self.SS[0]
+            m_vendors = min(self.SS[1:self.vendor_number + 1])
+            m_tec = min(self.SS[self.vendor_number + 1: self.vendor_number + self.tec_number + 1])
+            m_stec = min(self.SS[self.vendor_number + self.tec_number + 1:])
 
-            # elif min(*self.EL) < self.infinite:
-            #     #No more arrivals.
-                
+            m_min = min(self.SS)
 
+            if arriv == m_min:
+                self.system_time = arriv.arrival
+                choice = self.find_free_attender('vendor')
+                arriv.vendor_id = choice
+                arriv.vendor_end_time = self.system_time + generate_vendor_time()
+                self.SS[choice] = arriv
 
+                self.SS[0] = self.arrival_list.pop(0)
 
-    def _find_free_worker(self, s_type = 0):
-        _min = 1
-        _max = self.n_vendors + 1
-        if s_type:
-            _min += self.n_vendors
-            _max += self.n_tec
-            if s_type > 1:
-                _min += self.n_tec
-                _max += self.n_stec
-                
-        return [ i for i in range(_min,_max) if self.SS[i] == 0 ]
+            elif m_vendors == m_min:
+                self.system_time = m_vendors.vendor_end_time
 
-    def _route_client(self, id, _vendor_time):
-        _type = generate_client_type()
-
-        if _type == 4:
-            self.profit += 750
-            self.D.append((id, _vendor_time))
-        elif _type == 3:
-            stec = self._find_free_worker(s_type=2)
-            if stec:
-                choice = random.choice(stec)
-                # generate time
-                # add to SS variable
-            else:
-                self.Stec.put_nowait((id, _vendor_time, _type))
-        else:
-            tec = self._find_free_worker(s_type=1)
-            if tec:
-                choice = random.choice(tec)
-                # generate time
-                # add to SS variable
-            else:
-                stec = self._find_free_worker(s_type=2)
-                if stec:
-                    choice = random.choice(stec)
-                    # generate time
-                    # add to SS variable
+                if m_vendors.type == 4:
+                    m_vendors.finish_time = self.system_time
+                    #compra de equipo
+                    self.visitors_list.append(m_vendors)
                 else:
-                    self.Tec.put_nowait((id, _vendor_time, _type))
+                    choice = self.find_free_attender(attender_type[m_vendors.type])
+
+                    if choice:
+                        m_vendors.tec_id = choice
+                        m_vendors.tec_end_time = self.system_time + generate_tec_time()
+                        self.SS[m_vendors] = m_vendors
+                    else:
+                        if attender_type[m_vendors.type] == 'tec':
+                            # reparacion tipo 1 o 2
+                            self.tec_list.append(m_vendors)
+                        else:
+                            # cambio de equipo
+                            self.stec_list.append(m_vendors)
+
+                freeone = m_vendors.vendor_id
+
+                if len(self.vendor_list):
+                    newone = self.vendor_list.pop(0)
+                    newone.vendor_waiting_time = self.system_time - newone.arrival
+                    self.SS[freeone] = newone
+                
+                
+            elif m_tec == m_min:
+                self.system_time = m_tec.tec_end_time
+                m_tec.finish_time = self.system_time
+
+                freeone = m_tec.tec_id
+
+                self.visitors_list.append(m_tec)
+
+                if len(self.tec_list):
+                    nextone = self.tec_list.pop(0)
+                    nextone.tec_id = freeone
+                    nextone.tec_waiting_time = self.system_time - nextone.vendor_end_time
+                    self.SS[freeone] = nextone
 
 
+            elif m_stec == m_min:
+                self.system_time = m_stec.tec_end_time
+                m_stec.finish_time = self.system_time
 
-    def _generate_vendor_time(self):
-        return abs(normal(5,2))
+                freeone = m_stec.tec_id
 
+                self.visitors_list.append(m_stec)
 
-a = HappyComputing(480, 2, 1, 1,1/20, 20, 20,15, 9999999999)
+                if len(self.stec_list) or len(self.tec_list):
+                    nextone = self.stec_list.pop(0) if len(self.stec_list) else self.tec_list.pop(0)
+                    nextone.tec_id = freeone
+                    nextone.tec_waiting_time = self.system_time - nextone.vendor_end_time
+                    self.SS[freeone] = nextone
 
-a.simulate()
-
-print(a.A)
-print(a.D)
+            else:
+                print("anomalous exit")
+                break
+    
+    def find_free_attender(self, a_type):
+        choices = []
+        if a_type == 'vendor':
+            choices = self.SS[1:self.vendor_number+1]
+        # todo aquel que requiera el servicio de un tecnico puede ser atendido por el especializado o no
+        elif a_type == 'tec':
+            choices = self.SS[self.vendor_number+1:]
+        elif a_type == 'stec':
+            choices = self.SS[self.vendor_number + self.tec_number + 1:]
+        else:
+            print('bad call')
+            return 0
+        
+        candidates = []
+        for i in range(0,len(choices)):
+            if choices[i] == Client():
+                if a_type == 'vendor':
+                    candidates.append(i + 1)
+                elif a_type == 'tec':
+                    candidates.append(i + self.vendor_number + 1)
+                else:
+                    candidates.append(i + self.vendor_number + self.tec_number + 1)
+        
+        if len(candidates):
+            return random.choice(candidates)
+        return 0
